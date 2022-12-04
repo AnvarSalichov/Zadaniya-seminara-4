59854e4fc7753e4874db0108ca0eb595fc72b72d
Anvar <anv@list.com>
2022-12-04 23:21:28 +0400

    задание3 (массив из 8 элементов)

---
+++ work4_4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[]array = new int[8];   
+// FillArray(array);          
+// void FillArray(int[] collection)
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index]=new Random().Next(1,100);
+//         index++;
+//     }
+// }
+// Console.Write(String.Join(", ", array));
+// Console.WriteLine();
+
+int [] array = new int [8];
+GetArray(array);
+void GetArray(int [] massiv)
+{
+	for (int i = 0; i <massiv.Length; i++)
+	{
+	    massiv[i] = new Random().Next(0,100);
+	}
+}
+Console.Write(String.Join(", ", array));
+Console.WriteLine();
+
+
+ 
+
